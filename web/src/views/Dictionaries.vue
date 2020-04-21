a5cb887cc562941bc20cc8e744fae95f629073d0
darutoko <darutoko@gmail.com>
2020-04-21 18:12:28 +0700

    basic web files

---
+++ web/src/views/Dictionaries.vue
@@ -0,0 +1,16 @@
+<template>
+	<v-row>
+		<v-col>dictionaries</v-col>
+	</v-row>
+</template>
+
+<script>
+export default {
+	name: "Dictionaries",
+	data() {
+		return {
+			//
+		}
+	}
+}
+</script>
---
+++ web/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+	<v-row>
+		<v-col>
+			<router-link :to="{name: 'Dictionaries'}">Dictionaries</router-link>
+		</v-col>
+	</v-row>
+</template>
+
+<script>
+export default {
+	name: "Home",
+	components: {
+	}
+}
+</script>
