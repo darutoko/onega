c4eebb097b852eea8dfc223ff407a83f517e2b0c
darutoko <darutoko@gmail.com>
2020-05-09 14:37:40 +0700

    add card model

--- core/db/card.js
+++ core/db/card.js
@@ -1,18 +1,22 @@
 var pg = require("./pg")
 
 module.exports = {
-	getAll(params) {
-		return pg.query('select * from "card" where "deckId" = $1;', [params.deckId])
+	getAll({ deckId }) {
+		return pg.query('select * from "card" where "deckId" = $1;', [deckId])
 	},
-	create(params) {
+	create({ input, front, back, testByFront, deckId }) {
 		return pg.query('insert into "card" ("input", "front", "back", "testByFront", "deckId", "streak") values ($1, $2, $3, $4, $5, 0);', [
-			params.input,
-			params.front,
-			params.back,
-			params.testByFront,
-			params.deckId,
+			input,
+			front,
+			back,
+			testByFront,
+			deckId,
 		])
 	},
-	update() {},
-	delete() {},
+	update({ ids, newDeckId }) {
+		return pg.query('update "card" set "deckId" = $1 where "id" = any ($2)', [newDeckId, ids])
+	},
+	delete({ ids }) {
+		return pg.query('delete from "card" where "id" = any ($1)', [ids])
+	},
 }
